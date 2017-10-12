8b463615dc6e266c4c63a1e91edd0eb8ea76698d
Ari Becker <ari@mail.solacy.net>
2017-10-12 19:01:36 +0300

    changed to port 80

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -2,6 +2,9 @@ import static ratpack.groovy.Groovy.ratpack
 import java.net.InetAddress
 
 ratpack {
+    serverConfig {
+        port 80
+    }
     handlers {
         get {
             render InetAddress.localHost.hostName
